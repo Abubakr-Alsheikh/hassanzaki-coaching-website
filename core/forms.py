7b6d7c877824069d51939d086159b4fdc51ac4e5
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2024-12-02 20:28:00 +0300

    Removing Fields from Coaching Request Model

--- core/forms.py
+++ core/forms.py
@@ -11,10 +11,6 @@ class CoachingRequestForm(forms.ModelForm):
             'name',
             'email',
             'phone',
-            'guests',
-            'coaching_niche',
-            'revenue_target',
-            'roadblock',
             'referral_source'
         ]
 
@@ -53,28 +49,6 @@ class CoachingRequestForm(forms.ModelForm):
                 attrs={
                     'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500',
                     'placeholder': 'Type your phone'
-                }
-            ),
-            'guests': forms.NumberInput(
-                attrs={
-                    'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500',
-                    'placeholder': 'Number of Guests',
-                    'min': '0'  # Ensure guests cannot be negative
-                }
-            ),
-            'coaching_niche': forms.Select(
-                attrs={
-                    'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
-                }
-            ),
-            'revenue_target': forms.Select(
-                attrs={
-                    'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
-                }
-            ),
-            'roadblock': forms.Select(
-                attrs={
-                    'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
                 }
             ),
             'referral_source': forms.Select(
--- core/models.py
+++ core/models.py
@@ -4,30 +4,6 @@ from django.utils import timezone
 
 
 class CoachingRequest(models.Model):
-    COACHING_NICHES = (
-        ('life', 'Life'),
-        ('business', 'Business'),
-        ('dating', 'Dating'),
-        ('health', 'Health'),
-        ('career', 'Career'),
-        ('other', 'Other'),
-    )
-
-    REVENUE_TARGETS = (
-        ('5k-10k', '$5k - $10k / mo'),
-        ('10k-20k', '$10k - $20k / mo'),
-        ('20k-50k', '$20k - $50k / mo'),
-        ('50k-100k+', '$50k - $100k+ / mo'),
-    )
-
-    ROADBLOCKS = (
-        ('booking', 'Appointment Booking'),
-        ('closing', 'Closing Sales'),
-        ('retention', 'Client Retention'),
-        ('time', 'Time'),
-        ('other', 'Other'),
-    )
-
     REFERRAL_SOURCES = (
         ('instagram', 'Instagram'),
         ('linkedin', 'LinkedIn'),
@@ -46,11 +22,6 @@ class CoachingRequest(models.Model):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone = models.CharField("Phone", validators=[phone_regex], max_length=17, blank=False)  # validators should be a list
 
-
-    guests = models.IntegerField("Number of Guests", default=0) # Assuming guests can be 0 or more
-    coaching_niche = models.CharField("Coaching Niche", max_length=10, choices=COACHING_NICHES)
-    revenue_target = models.CharField("Revenue Target", max_length=10, choices=REVENUE_TARGETS)
-    roadblock = models.CharField("Roadblock", max_length=10, choices=ROADBLOCKS)
     referral_source = models.CharField("Referral Source", max_length=10, choices=REFERRAL_SOURCES)
 
 
