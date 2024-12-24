e7c408a3e473e004c586899b6d67d52950da2851
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2024-12-24 21:23:07 +0300

    Adding production and development files

--- coaching_website/wsgi.py
+++ coaching_website/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coaching_website.settings')
+os.environ.setdefault('DJANGO_ENV', 'production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coaching_website.settings.production')
 
 application = get_wsgi_application()
