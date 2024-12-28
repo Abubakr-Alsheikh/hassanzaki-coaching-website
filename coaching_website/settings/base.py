38bf586ee02bb7f0e5b308e5256902eaffdaab1c
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2024-12-28 12:58:30 +0300

    Adding dotevn, securing the secret key

--- coaching_website/settings/base.py
+++ coaching_website/settings/base.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$u!hvpydl@wy%y*sxhdd3p5ecau1d_u2p^yys8879^yhustdj0'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # Application definition
 INSTALLED_APPS = [
