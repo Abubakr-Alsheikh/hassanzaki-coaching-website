909bd1d128e99dfc64ecc62b97b9fcf9b3ff23ad
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2024-12-28 18:00:19 +0300

    Adding animations using AOS

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def sub(value, arg):
     return value - arg
+
+@register.filter(name='multiply_by')
+def multiply_by(value, arg):
+    return int(value) * arg
