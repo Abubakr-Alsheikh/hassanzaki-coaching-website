7b340bac3d6867d0a59436410a7e26288749ab00
Abubakr-Alsheikh <aubakr.5230@gmail.com>
2024-12-31 18:56:38 +0300

    Adjust the prononce

--- core/forms.py
+++ core/forms.py
@@ -94,7 +94,7 @@ class CoachingRequestForm(forms.ModelForm):
                 attrs={
                     "rows": "4",
                     "class": "block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500",
-                    "placeholder": "شاركنا بتفاصيل حول ما تأمل تحقيقه من الجلسة (اختياري)",
+                    "placeholder": "شاركني بتفاصيل حول ما تأمل تحقيقه من الجلسة (اختياري)",
                 }
             ),
         }
